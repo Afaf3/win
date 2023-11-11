10b2f416cf9af47d7bd45be0ebd726a2a8303222
Afaf3 <TU3755532@taibahu.edu.sa>
2023-11-11 22:38:37 +0300

    first commit

---
+++ inc/db.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection parameters
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "win";
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+?>
